c3ea3c6a38c92bba92f368219bf78b5990f294f1
zieezie23-rgb <zieezie23@gmail.com>
2026-08-20 00:38:35 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='uang-kas-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
