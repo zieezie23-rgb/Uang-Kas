703e1120373681f3218a16bea36f787708dee8bb
zie <zieezie23@gmail.com>
2026-09-07 13:15:32 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='uang-kas-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
